c3c9b68594401861ec1cde14be30f2eaeb0465b2
Tyler Huang <thuang00@stuy.edu>
2019-02-13 08:23:34 -0500

    added toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,8 +3,22 @@ public class KnightBoard{
   private int[][] board;
 
   public KnightBoard(int startingRows, int startingCols){
+    if (startingRows < 0 || startingCols < 0) throw new IllegalArgumentException();
     board = new int[startingRows][startingCols];
   }
-
-
+  //string representation of the board
+  public String toString(){
+    String output = "";
+    for (int r = 0; r < board.length; r++){
+      for (int c = 0; c < board[r].length;r++){
+        if (board[r][c] < 10){
+          output += " " + board[r][c] + " ";
+        }
+        else{
+          output += board[r][c] + " ";
+        }
+      }
+    }
+    return output;
+  }
 }
