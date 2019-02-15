6c18eb2198ba6353816165587c9188c321271087
Tyler Huang <thuang00@stuy.edu>
2019-02-15 14:48:28 -0500

    did not work

--- KnightBoard.java
+++ KnightBoard.java
@@ -83,13 +83,16 @@ public class KnightBoard{
   public int countSolutions(int row, int col, int level){
     int count = 0;
     if (level == board.length * board[0].length + 1){
-      return count;
+      return 1;
     }
     int[] v = new int[]{1,2,1,-2,-1,2,-1,-2,2,-1,2,1,-2,1,-2,-1};
     for (int i = 0; i < v.length; i+=2){
-      if (board[row][col] == 0){
-        board[row][col] = level;
-        count += countSolutions(level+1, row+v[i], col+v[i+1]);
+      if (checkBounds(row,col)){
+        if (board[row][col] == 0){
+          board[row][col] = level;
+          count += countSolutions(level+1, row+v[i], col+v[i+1]);
+          board[row][col] = 0;
+        }
       }
     }
     return count;
@@ -99,7 +102,8 @@ public class KnightBoard{
     //k.board[0][1] = 10;
     //k.board[0][2] = 1;
     //System.out.print(k.toString());
-    System.out.println(k.solve(0,0));
+    //System.out.println(k.solve(0,0));
+    System.out.println(k.countSolutions(0,0));
     System.out.println(k.toString());
   }
 }
